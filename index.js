d32d657e0962201debee5c69454a914cb84aadee
SeryiBaran <seryibaranminepy@yandex.ru>
2022-03-07 16:46:34 +0300

    Эксперимент

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
+const axios = require("axios");
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
+let a;
+
+async function fetchData() {
+    try {
+        const [response] = await Promise.all([
+            axios.get("https://disease.sh/v3/covid-19/countries?sort=cases"),
+        ]);
+
+        a = response.data;
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+fetchData();
+
+app.get("/api", (req, res) => {
+    res.send(a);
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
 });
 
 app.listen(3000, () => {
-    console.log('Application listening on port 3000!');
+    console.log("Application listening on port 3000!");
 });
