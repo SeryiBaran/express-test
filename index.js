b2b94148c605dbde22f289201749e420a941ef80
SeryiBaran <seryibaranminepy@yandex.ru>
2022-06-16 11:03:36 +0300

    CJS to ESM

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const path = require("path");
-const fs = require("fs");
-const axios = require("axios");
-const app = express();
+import express from "express";
+import bodyParser from "body-parser";
+import path from "path";
+import fs from "fs";
+import axios from "axios";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
 const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, "/public")));
@@ -14,43 +19,44 @@ app.use(bodyParser.json());
 let api_data;
 
 async function fetchData() {
-  try {
-    await axios
-      .get("https://disease.sh/v3/covid-19/countries?sort=cases")
-      .then((res) => {
-        api_data = res.data;
-      });
-  } catch (e) {
-    console.log(e);
-  }
+    try {
+        await axios
+            .get("https://disease.sh/v3/covid-19/countries?sort=cases")
+            .then((res) => {
+            api_data = res.data;
+        });
+    }
+    catch (e) {
+        console.log(e);
+    }
 }
 
 fetchData();
 
 app.get("/api", async ({ res }) => {
-  await fetchData();
-  res.send(api_data);
+    await fetchData();
+    res.send(api_data);
 });
 
 app.get("/", ({ res }) => {
-  res.sendFile(`${__dirname}/public/index.html`);
+    res.sendFile(`${__dirname}/public/index.html`);
 });
 
 app.get("/void", ({ res }) => {
-  res.sendFile(`${__dirname}/public/void.html`);
+    res.sendFile(`${__dirname}/public/void.html`);
 });
 
 app.post("/ascii", async (req, res) => {
-  if (req.body.lol) {
-    let data = await fs.readFileSync("ascii.txt", "utf8");
-    res.send(data.toString());
-  }
+    if (req.body.lol) {
+        let data = await fs.readFileSync("ascii.txt", "utf8");
+        res.send(data.toString());
+    }
 });
 
 app.post("/test", (req) => {
-  console.log(req.body);
+    console.log(req.body);
 });
 
 app.listen(PORT, () => {
-  console.log(`Application listening on port ${PORT}!`);
+    console.log(`Application listening on port ${PORT}!`);
 });
